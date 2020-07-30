5e980781cbe6c099fba487a4215fc682a776a468
Alejandro Cantillo <jaimecantillo95@gmail.com>
2020-07-30 15:47:15 -0500

    MOD mediaplayer, index.css, plugins

--- assets/plugins/Ads/index.ts
+++ assets/plugins/Ads/index.ts
@@ -1,27 +1,30 @@
 import MediaPlayer from '../../MediaPlayer';
-import Ads, { Ad } from './Ads'
+import Ads, { Ad } from './Ads';
 
-class AdsPlugung {
+class AdsPlugin {
   private ads: Ads;
   private player: MediaPlayer;
   private media: HTMLMediaElement;
   private currentAd: Ad;
+  private adsContainer: HTMLElement;
 
-  constructor () {
+  constructor() {
     this.ads = Ads.getInstance();
+    this.adsContainer = document.createElement('div');
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
   }
 
   run(player: MediaPlayer) {
     this.player = player;
+    this.player.container.appendChild(this.adsContainer);
     this.media = this.player.media;
     this.media.addEventListener('timeupdate', this.handleTimeUpdate);
   }
 
   private handleTimeUpdate() {
     const currentTime = Math.floor(this.media.currentTime);
-    if(currentTime % 30 === 0) {
-      this.renderAd()
+    if (currentTime % 30 === 0) {
+      this.renderAd();
     }
   }
 
@@ -29,10 +32,26 @@ class AdsPlugung {
     if (this.currentAd) {
       return;
     }
+
     const ad = this.ads.getAd();
     this.currentAd = ad;
-    console.log(this.currentAd);
+    this.adsContainer.innerHTML = `
+      <div class="ads">
+        <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+          <img class="ads__img" src="${this.currentAd.imageUrl}" />
+          <div class="ads__info">
+            <h5 class="ads__title">${this.currentAd.title}</h5>
+            <p class="ads__body">${this.currentAd.body}</p>
+          </div>
+        </a>
+      </div>
+    `;
+
+    setTimeout(() => {
+      this.currentAd = null;
+      this.adsContainer.innerHTML = '';
+    }, 10000);
   }
 }
 
-export default AdsPlugung;
+export default AdsPlugin;
